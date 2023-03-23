fbcbfdedd5ea17d2691e58cdfc1b07f8d04ec6d3
lyfhael <singinganthlers@hotmail.com>
2023-03-23 20:11:10 +0100

    so_long termine/ update libft

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: hateisse <hateisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 17:31:29 by hateisse          #+#    #+#             */
-/*   Updated: 2023/03/23 17:40:08 by hateisse         ###   ########.fr       */
+/*   Updated: 2023/03/23 20:04:06 by hateisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 # define PIPEX_H
 # include "libft.h"
-
+# include <stdio.h>
 typedef struct s_params
 {
 	int	fd1;
--- pipex/srcs/main.c
+++ pipex/srcs/main.c
@@ -6,16 +6,18 @@
 /*   By: hateisse <hateisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 17:29:05 by hateisse          #+#    #+#             */
-/*   Updated: 2023/03/23 18:13:19 by hateisse         ###   ########.fr       */
+/*   Updated: 2023/03/23 20:05:08 by hateisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	ft_error(int errno)
+void	ft_error(int erno)
 {
-	if (errno == 0)
+	if (erno == 0)
 		ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 file2\n", 2);
+	if (erno == 1)
+		perror("Error");
 	exit(1);
 }
 
@@ -25,7 +27,7 @@ int	open_output(char *filename)
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
 	if (fd == -1)
-		ft_error()
+		ft_error(1);
 }
 
 int	main(int argc, char **argv)
